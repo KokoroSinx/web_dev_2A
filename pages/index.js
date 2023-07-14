4cad18789e57466f69534e37f76982b30d221697
Shionji <851912629@qq.com>
2023-07-14 13:47:14 +0900

    qq

---
+++ pages/index.js
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+
+export default function TodoList() {
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const addTodo = () => {
+    if (inputValue.trim() !== '') {
+      setTodos([...todos, inputValue]);
+      setInputValue('');
+    }
+  };
+
+  const deleteTodo = (index) => {
+    const updatedTodos = todos.filter((_, i) => i !== index);
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <div>
+      <h1>Todo List Manager</h1>
+      <input type="text" value={inputValue} onChange={handleInputChange} />
+      <button onClick={addTodo}>Add</button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <button onClick={() => deleteTodo(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
